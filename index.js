c19a24aea672d4ec337aa5768414b3097cc8f7cc
Abhishek Mishra <abhishek@digiqt>
2022-07-02 10:41:55 +0530

    First COmmit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+
+const PORT = 3050;
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Healthy");
+})
+
+
+app.listen(PORT,()=>{
+    console.log("Process Running; Server Started");
+});
